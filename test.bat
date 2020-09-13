e23d6d848b6b725e32e155fc4b9b047402045733
arlbibek <53701945+arlbibek@users.noreply.github.com>
2020-09-13 22:28:43 +0545

    Replaced these files

--- test.bat
+++
@@ -1,7 +0,0 @@
-::Test bat file
-:: @echo off
-powershell
-echo "Downloading Firefox"
-pause
-powershell -Command "Invoke-WebRequest https://www.mozilla.org/en-US/firefox/download/thanks/ -Outfile firefox.exe"
-cls
